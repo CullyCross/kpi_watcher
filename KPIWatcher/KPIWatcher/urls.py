fd0915952edcd0bc847c2a5f9bde8cdc34df244f
CullyCross <elaugfein@gmail.com>
2015-06-09 00:38:13 +0300

    - Fix login bug

--- KPIWatcher/KPIWatcher/urls.py
+++ KPIWatcher/KPIWatcher/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('ratings.urls')),
     url(r'^events/', include('events.urls')),
-    url(r'^login/$', 'django.contrib.auth.views.login', name="login"),
+    url(r'^login/$', 'django.contrib.auth.views.login', name="login", kwargs={'template_name': 'post_login.html'}),
     url(r'^logout/$', 'django.contrib.auth.views.logout'),
 )
