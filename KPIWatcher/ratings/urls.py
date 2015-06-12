1149c5200e30d75bcec6303507aeffcb1d5afa36
CullyCross <elaugfein@gmail.com>
2015-06-12 03:43:58 +0300

    - Voting for groups - Permissions to vote

--- KPIWatcher/ratings/urls.py
+++ KPIWatcher/ratings/urls.py
@@ -5,9 +5,12 @@ urlpatterns = [
     url(r'^$', views.top_ratings, name="all_teachers"),
     url(r'^teacher/(?P<pk>[0-9]+)/$', views.teacher_page, name="teacher_page"),
     url(r'^teacher/(?P<pk>[0-9]+)/vote/$', views.vote, name="teacher_vote_url"),
-    url(r'^students/$', views.students_all, name="all_student"),
+    url(r'^students/$', views.students_all, name="all_students"),
     url(r'^students/(?P<pk>[0-9]+)/$', views.student_page, name="student_page"),
     url(r'^choose_your_hero/', views.basic_register, name="basic_register"),
     url(r'^registration/', views.registration, name="registration"),
     url(r'^university/', views.university, name="university"),
+    url(r'^groups/(?P<pk>[0-9]+)/$', views.group_page, name="group_page"),
+    url(r'^groups/(?P<pk>[0-9]+)/vote/$', views.vote_group, name="group_vote_url"),
+    url(r'^groups/', views.all_groups, name="all_groups"),
 ]
--- KPIWatcher/ratings/views.py
+++ KPIWatcher/ratings/views.py
@@ -7,7 +7,7 @@ from .models import *
 from ratings.forms import TeacherDetailsForm, StudentDetailsForm, UserDetailsForm
 
 
-#todo: unsubscribe event, group page (and vote also)
+#todo: group page (and vote also)
 #todo: links in university page, etc
 
 def top_ratings(request):
@@ -19,10 +19,14 @@ def teacher_page(request, pk):
     teacher = get_object_or_404(Teacher, pk=pk)
     if hasattr(request.user, 'student'):
         perm_to_vote = True
+        perm_to_comment = True
     else:
         perm_to_vote = False
+        perm_to_comment = False
 
-    return render(request, 'ratings/teacher_page.html', {'teacher': teacher, 'range': range(1, 11), 'perm_to_vote': perm_to_vote})
+    return render(request, 'ratings/teacher_page.html',
+                  {'teacher': teacher, 'range': range(1, 11),
+                   'perm_to_vote': perm_to_vote, 'perm_to_comment': perm_to_comment})
 
 
 def vote(request, pk):
@@ -30,15 +34,19 @@ def vote(request, pk):
     if request.POST and not request.user.is_anonymous():
         if hasattr(request.user, 'student'):
             voted = teacher.vote(int(request.POST['vote']), request.user.student)
+            perm_to_comment = True
             if voted:
                 message = "Success"
             else:
                 message = "You've already voted"
         else:
+            perm_to_comment = False
             message = "You can't vote!"
     else:
         message = "Error!"
-    return render(request, 'ratings/teacher_page.html', {'teacher': teacher, 'range': range(1, 11), 'message': message})
+    return render(request, 'ratings/teacher_page.html',
+                  {'teacher': teacher, 'range': range(1, 11),
+                   'message': message, 'perm_to_comment': perm_to_comment})
 
 
 def student_page(request, pk):
@@ -133,3 +141,54 @@ def university(request):
     kpi = University.objects.all().first()
     kpi.count_rating()
     return render(request, 'ratings/university.html', {'university': kpi})
+
+
+def all_groups(request):
+    groups_all = Group.objects.all()
+    paginator = Paginator(groups_all, 25)
+
+    page = request.GET.get('page')
+
+    try:
+        groups = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        groups = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        groups = paginator.page(paginator.num_pages)
+
+    return render(request, 'ratings/all_groups.html', {'groups': groups})
+
+
+def group_page(request, pk):
+    group = get_object_or_404(Group, pk=pk)
+    if hasattr(request.user, 'teacher'):
+        perm_to_vote = True
+        perm_to_comment = True
+    else:
+        perm_to_vote = False
+        perm_to_comment = False
+
+    return render(request, 'ratings/group_page.html',
+                  {'group': group, 'range': range(1, 11),
+                   'perm_to_vote': perm_to_vote, 'perm_to_comment': perm_to_comment})
+
+def vote_group(request, pk):
+    group = get_object_or_404(Group, pk=pk)
+    if request.POST and not request.user.is_anonymous():
+        if hasattr(request.user, 'teacher'):
+            perm_to_comment = True
+            voted = group.vote(int(request.POST['vote']), request.user.teacher)
+            if voted:
+                message = "Success"
+            else:
+                message = "You've already voted"
+        else:
+            perm_to_comment = False
+            message = "You can't vote!"
+    else:
+        message = "Error!"
+    return render(request, 'ratings/group_page.html',
+                  {'group': group, 'range': range(1, 11),
+                   'message': message, 'perm_to_comment': perm_to_comment})
